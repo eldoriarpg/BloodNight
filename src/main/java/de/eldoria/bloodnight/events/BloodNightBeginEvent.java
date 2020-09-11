e0a9f50c281522479476c81d9854914ab1d0c7af
RainbowDashLabs <mail@code-fox.de>
2020-09-11 19:17:21 +0200

    add Events

---
+++ src/main/java/de/eldoria/bloodnight/events/BloodNightBeginEvent.java
@@ -0,0 +1,34 @@
+package de.eldoria.bloodnight.events;
+
+import lombok.Getter;
+import org.bukkit.World;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.world.WorldEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Event which is fired when a blood nights begins.
+ */
+@Getter
+public class BloodNightBeginEvent extends WorldEvent {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    /**
+     * Create a new Blood Night Begin Event.
+     *
+     * @param world world where the blood night has begun.
+     */
+    public BloodNightBeginEvent(World world) {
+        super(world);
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+}
