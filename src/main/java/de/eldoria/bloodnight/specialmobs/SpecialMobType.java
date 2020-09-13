e9129b0c5c0695ced321e88eff096d3d8d261379
RainbowDashLabs <mail@code-fox.de>
2020-09-14 00:24:47 +0200

    add mob class reference to special mobs

--- src/main/java/de/eldoria/bloodnight/specialmobs/SpecialMobType.java
+++ src/main/java/de/eldoria/bloodnight/specialmobs/SpecialMobType.java
@@ -26,7 +26,6 @@ import de.eldoria.bloodnight.specialmobs.mobs.zombie.SpeedZombie;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Enderman;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Phantom;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Slime;
@@ -35,7 +34,10 @@ import org.bukkit.entity.Witch;
 import org.bukkit.entity.Zombie;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -43,111 +45,112 @@ public enum SpecialMobType {
     /**
      * Creeper with speed 4 and faster explosion
      */
-    SPEED_CREEPER(EntityType.CREEPER, e -> new SpeedCreeper((Creeper) e)),
+    SPEED_CREEPER(Creeper.class, e -> new SpeedCreeper((Creeper) e)),
     /**
      * Creeper which leaves a poisinous field after explosion
      */
-    TOXIC_CREEPER(EntityType.CREEPER, e -> new ToxicCreeper((Creeper) e)),
+    TOXIC_CREEPER(Creeper.class, e -> new ToxicCreeper((Creeper) e)),
     /**
      * Powered creeper with faster explosion.
      */
-    NERVOUS_POWERED_CREEPER(EntityType.CREEPER, e -> new NervousPoweredCreeper((Creeper) e)),
+    NERVOUS_POWERED_CREEPER(Creeper.class, e -> new NervousPoweredCreeper((Creeper) e)),
     /**
      * Explodes when on fire.
      */
-    UNSTABLE_CREEPER(EntityType.CREEPER, e -> new UnstableCreeper((Creeper) e)),
+    UNSTABLE_CREEPER(Creeper.class, e -> new UnstableCreeper((Creeper) e)),
     /**
      * Creeper on a invisible bat.
      */
-    FLYING_CREEPER(EntityType.CREEPER, e -> new FlyingCreeper((Creeper) e)),
+    FLYING_CREEPER(Creeper.class, e -> new FlyingCreeper((Creeper) e)),
     /**
      * Creeper which can teleport to its target every 10 seconds.
      */
-    ENDER_CREEPER(EntityType.CREEPER, e -> new EnderCreeper((Creeper) e)),
-    /**
-     * A wither skeleton riding a cave spider
-     */
-    WITHER_SKELETON_RIDER(EntityType.SPIDER, e -> new WitherSkeletonRider((Spider) e)),
+    ENDER_CREEPER(Creeper.class, e -> new EnderCreeper((Creeper) e)),
     /**
      * A zombie with speed 4
      */
-    SPEED_ZOMBIE(EntityType.ZOMBIE, e -> new SpeedZombie((Zombie) e)),
+    SPEED_ZOMBIE(Zombie.class, e -> new SpeedZombie((Zombie) e)),
     /**
      * A zombie with a sword and invisbility effect
      */
-    INVISIBLE_ZOMBIE(EntityType.ZOMBIE, e -> new InvisibleZombie((Zombie) e)),
+    INVISIBLE_ZOMBIE(Zombie.class, e -> new InvisibleZombie((Zombie) e)),
     /**
      * A zombie with a full armor.
      */
-    ARMORED_ZOMBIE(EntityType.ZOMBIE, e -> new ArmoredZombie((Zombie) e)),
+    ARMORED_ZOMBIE(Zombie.class, e -> new ArmoredZombie((Zombie) e)),
     /**
      * A witch throwing fireballs instead of potions.
      */
-    FIRE_WIZARD(EntityType.WITCH, e -> new FireWizard((Witch) e)),
+    FIRE_WIZARD(Witch.class, e -> new FireWizard((Witch) e)),
     /**
      * A witch throwing lightnings
      */
-    THUNDER_WIZARD(EntityType.WITCH, e -> new ThunderWizard((Witch) e)),
+    THUNDER_WIZARD(Witch.class, e -> new ThunderWizard((Witch) e)),
     /**
      * A witch applying wither effect on players.
      */
-    WITHER_WIZARD(EntityType.WITCH, e -> new WitherWizard((Witch) e)),
+    WITHER_WIZARD(Witch.class, e -> new WitherWizard((Witch) e)),
     /**
      * A skeleton with tipped arrows.
      */
-    MAGIC_SKELETON(EntityType.SKELETON, e -> new MagicSkeleton((Skeleton) e)),
+    MAGIC_SKELETON(Skeleton.class, e -> new MagicSkeleton((Skeleton) e)),
     /**
      * A skeleton with invisibility
      */
-    INVISIBLE_SKELETON(EntityType.SKELETON, e -> new InvisibleSkeleton((Skeleton) e)),
-    // Enderman are always angry.
+    INVISIBLE_SKELETON(Skeleton.class, e -> new InvisibleSkeleton((Skeleton) e)),
     /**
      * A enderman which leaves a cloud of poision after teleport.
      */
-    TOXIC_ENDERMAN(EntityType.ENDERMAN, e -> new ToxicEnderman((Enderman) e)),
+    TOXIC_ENDERMAN(Enderman.class, e -> new ToxicEnderman((Enderman) e)),
     /**
      * Applies blindness effect hit.
      */
-    FEARFULL_ENDERMAN(EntityType.ENDERMAN, e -> new FearfullEnderman((Enderman) e)),
-    /**
-     * A skeleton on a fast spider.
-     */
-    SPEED_SKELETON_RIDER(EntityType.SPIDER, e -> new SpeedSkeletonRider((Spider) e)),
+    FEARFULL_ENDERMAN(Enderman.class, e -> new FearfullEnderman((Enderman) e)),
     /**
      * A slime which explodes in a cloud of poison when killed.
      */
-    TOXIC_SLIME(EntityType.SLIME, e -> new ToxicSlime((Slime) e)),
+    TOXIC_SLIME(Slime.class, e -> new ToxicSlime((Slime) e)),
     /**
      * A phantom with invisibility and glowing effect.
      */
-    PHANTOM_SOUL(EntityType.PHANTOM, e -> new PhantomSoul((Phantom) e)),
+    PHANTOM_SOUL(Phantom.class, e -> new PhantomSoul((Phantom) e)),
     /**
      * Applies a slowness effect on hit.
      */
-    FEARFULL_PHANTOM(EntityType.PHANTOM, e -> new FearfulPhantom((Phantom) e)),
+    FEARFULL_PHANTOM(Phantom.class, e -> new FearfulPhantom((Phantom) e)),
     /**
      * Phantom with a blaze on top.
      */
-    FIRE_PHANTOM(EntityType.PHANTOM, e -> new FirePhantom((Phantom) e)),
+    FIRE_PHANTOM(Phantom.class, e -> new FirePhantom((Phantom) e)),
+    /**
+     * A skeleton on a fast spider.
+     */
+    SPEED_SKELETON_RIDER(Spider.class, e -> new SpeedSkeletonRider((Spider) e)),
+    /**
+     * A wither skeleton riding a cave spider
+     */
+    WITHER_SKELETON_RIDER(Spider.class, e -> new WitherSkeletonRider((Spider) e)),
     /**
      * A spider with a blaze.
      */
-    BLAZE_RIDER(EntityType.SPIDER, e -> new BlazeRider((Spider) e));
+    BLAZE_RIDER(Spider.class, e -> new BlazeRider((Spider) e));
 
-    private final EntityType baseEntityType;
+    private final Class<? extends Entity> baseEntity;
     private final Function<Entity, SpecialMob> create;
 
-    SpecialMobType(EntityType baseEntityType, Function<Entity, SpecialMob> create) {
-        this.baseEntityType = baseEntityType;
+    SpecialMobType(Class<? extends Entity> baseEntity, Function<Entity, SpecialMob> create) {
+        this.baseEntity = baseEntity;
         this.create = create;
     }
+
+    private static final Map<Class<? extends Entity>, List<SpecialMobType>> MAPPINGS = new HashMap<>();
 
     /**
      * Creates the requested special mob
      *
      * @param entity base entity for special mob
      * @return SpecialMob
-     * @throws ClassCastException when {@link Entity#getType()} is not equals {@link SpecialMobType#getBaseEntityType()} or a child of this type
+     * @throws ClassCastException when {@link Entity} is not equals {@link SpecialMobType#getBaseEntity()} or a child of this type
      */
     public SpecialMob create(Entity entity) throws ClassCastException {
         return create.apply(entity);
@@ -159,11 +162,16 @@ public enum SpecialMobType {
      * @param entity Entity to get a special mob
      * @return List of matchin entities for this entity. May be empty.
      */
-    private static List<SpecialMobType> getMatchingTypes(Entity entity) {
-        return Arrays.stream(values()).filter(t -> t.baseEntityType == entity.getType()).collect(Collectors.toList());
+    public static List<SpecialMobType> getMatchingTypes(Entity entity) {
+        Class<? extends Entity> entityClass = entity.getType().getEntityClass();
+        if (entityClass == null) return Collections.emptyList();
+
+        return MAPPINGS.computeIfAbsent(entityClass, e -> Arrays.stream(values())
+                .filter(t -> t.baseEntity.isAssignableFrom(entityClass))
+                .collect(Collectors.toList()));
     }
 
-    public EntityType getBaseEntityType() {
-        return baseEntityType;
+    public Class<? extends Entity> getBaseEntity() {
+        return baseEntity;
     }
 }
