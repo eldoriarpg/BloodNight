092917af488394ec8eca39449a3d4e677732b3ee
RainbowDashLabs <mail@code-fox.de>
2020-09-13 11:02:30 +0200

    add custom skeletons

---
+++ src/main/java/de/eldoria/bloodnight/specialmobs/mobs/skeleton/AbstractSkeleton.java
@@ -0,0 +1,22 @@
+package de.eldoria.bloodnight.specialmobs.mobs.skeleton;
+
+import de.eldoria.bloodnight.specialmobs.SpecialMob;
+import de.eldoria.bloodnight.specialmobs.SpecialMobUtil;
+import lombok.Getter;
+import org.bukkit.Particle;
+import org.bukkit.entity.Skeleton;
+
+public class AbstractSkeleton implements SpecialMob {
+    @Getter
+    private final Skeleton skeleton;
+
+    public AbstractSkeleton(Skeleton skeleton) {
+        this.skeleton = skeleton;
+    }
+
+    @Override
+    public void onEnd() {
+        SpecialMobUtil.spawnParticlesAround(skeleton, Particle.CAMPFIRE_COSY_SMOKE, 30);
+        skeleton.remove();
+    }
+}
