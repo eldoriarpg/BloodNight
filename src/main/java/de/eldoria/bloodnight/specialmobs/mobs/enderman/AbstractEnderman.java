95216d2fd373e5cd20d11be305f5a72a92e39bd5
RainbowDashLabs <mail@code-fox.de>
2020-09-13 12:10:46 +0200

    add special enderman

---
+++ src/main/java/de/eldoria/bloodnight/specialmobs/mobs/enderman/AbstractEnderman.java
@@ -0,0 +1,39 @@
+package de.eldoria.bloodnight.specialmobs.mobs.enderman;
+
+import de.eldoria.bloodnight.specialmobs.SpecialMob;
+import de.eldoria.bloodnight.specialmobs.SpecialMobUtil;
+import lombok.Getter;
+import org.bukkit.Particle;
+import org.bukkit.entity.Enderman;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class AbstractEnderman implements SpecialMob {
+    @Getter
+    private final Enderman enderman;
+
+    public AbstractEnderman(Enderman enderman) {
+        this.enderman = enderman;
+    }
+
+    @Override
+    public void tick() {
+        if (enderman.getTarget() != null) return;
+        Collection<Entity> nearbyPlayers = enderman.getWorld().
+                getNearbyEntities(enderman.getLocation(), 16, 16, 16, e -> e.getType() == EntityType.PLAYER);
+        if (nearbyPlayers.isEmpty()) {
+            return;
+        }
+        enderman.setTarget((LivingEntity) new ArrayList<>(nearbyPlayers).get(0));
+    }
+
+    @Override
+    public void onEnd() {
+        SpecialMobUtil.spawnParticlesAround(enderman, Particle.DRAGON_BREATH, 10);
+        enderman.remove();
+    }
+}
