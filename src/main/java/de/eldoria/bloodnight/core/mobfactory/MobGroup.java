0f502355d977f9f3c726dd726945f8cc7d12e9dd
RainbowDashLabs <mail@code-fox.de>
2020-09-24 00:33:19 +0200

    add mob group to group mobs of same type

---
+++ src/main/java/de/eldoria/bloodnight/core/mobfactory/MobGroup.java
@@ -0,0 +1,34 @@
+package de.eldoria.bloodnight.core.mobfactory;
+
+import de.eldoria.bloodnight.specialmobs.SpecialMob;
+import lombok.Getter;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class MobGroup {
+    @Getter
+    private final EntityType entityType;
+    @Getter
+    private final List<MobFactory> factories = new ArrayList<>();
+
+    public MobGroup(EntityType entityType) {
+        this.entityType = entityType;
+    }
+
+    public Class<? extends Entity> getBaseClass() {
+        return entityType.getEntityClass();
+    }
+
+    void registerFactory(MobFactory factory) {
+        factories.add(factory);
+    }
+
+    public void registerFactory(String displayName, Function<LivingEntity, SpecialMob> factory) {
+        factories.add(new MobFactory(entityType, displayName, factory));
+    }
+}
