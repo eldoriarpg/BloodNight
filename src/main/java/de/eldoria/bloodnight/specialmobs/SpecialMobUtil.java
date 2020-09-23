f5817ccb9d1f3a21a1c7db1dccb0bed85b922eda
RainbowDashLabs <mail@code-fox.de>
2020-09-24 00:29:19 +0200

    mark old methods as deprecated and add replacements

--- src/main/java/de/eldoria/bloodnight/specialmobs/SpecialMobUtil.java
+++ src/main/java/de/eldoria/bloodnight/specialmobs/SpecialMobUtil.java
@@ -1,10 +1,13 @@
 package de.eldoria.bloodnight.specialmobs;
 
+import de.eldoria.bloodnight.core.BloodNight;
 import de.eldoria.bloodnight.util.VectorUtil;
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.World;
+import org.bukkit.entity.AreaEffectCloud;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -13,6 +16,9 @@ import org.bukkit.entity.Projectile;
 import org.bukkit.entity.ThrownPotion;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
@@ -26,10 +32,12 @@ public final class SpecialMobUtil {
     private SpecialMobUtil() {
     }
 
+    @Deprecated
     public static void spawnLingeringPotionAt(Location location, PotionEffect potionEffect) {
         spawnPotionAt(location, potionEffect, Material.LINGERING_POTION);
     }
 
+    @Deprecated
     public static void spawnPotionAt(Location location, PotionEffect potionEffect, Material potionType) {
         ItemStack potion = new ItemStack(potionType);
         PotionMeta potionMeta = (PotionMeta) potion.getItemMeta();
@@ -43,24 +51,43 @@ public final class SpecialMobUtil {
     }
 
     public static void addPotionEffect(LivingEntity entity, PotionEffectType type, int amplifier, boolean visible) {
-        entity.addPotionEffect(new PotionEffect(type, 60, amplifier, visible, visible));
+        entity.addPotionEffect(new PotionEffect(type, 60 * 20, amplifier, visible, visible));
     }
 
+    @Deprecated
     public static void spawnParticlesAround(Entity entity, Particle particle, int amount) {
         spawnParticlesAround(entity.getLocation(), particle, amount);
     }
 
+    @Deprecated
     public static void spawnParticlesAround(Location location, Particle particle, int amount) {
         World world = location.getWorld();
         for (int i = 0; i < amount; i++) {
-            world.spawnParticle(particle,
-                    location.clone()
-                            .add(
-                                    RAND.nextDouble(-3, 3),
-                                    RAND.nextDouble(-3, 3),
-                                    RAND.nextDouble(-3, 3)),
-                    1);
+            switch (particle) {
+                case REDSTONE:
+                    world.spawnParticle(particle,
+                            location.clone()
+                                    .add(
+                                            RAND.nextDouble(-3, 3),
+                                            RAND.nextDouble(-3, 3),
+                                            RAND.nextDouble(-3, 3)),
+                            1, new Particle.DustOptions(Color.RED, 2));
+                    break;
+                default:
+                    world.spawnParticle(particle,
+                            location.clone()
+                                    .add(
+                                            RAND.nextDouble(-3, 3),
+                                            RAND.nextDouble(-3, 3),
+                                            RAND.nextDouble(-3, 3)),
+                            1);
+            }
         }
+    }
+
+    public static void spawnEffectArea(Location location, PotionData potionData) {
+        AreaEffectCloud entity = (AreaEffectCloud) location.getWorld().spawnEntity(location, EntityType.AREA_EFFECT_CLOUD);
+        entity.setBasePotionData(potionData);
     }
 
     public static void launchProjectileOnTarget(Mob source, Class<? extends Projectile> projectile, double speed) {
@@ -78,15 +105,40 @@ public final class SpecialMobUtil {
 
     @SuppressWarnings("unchecked")
     public static <T extends Entity> T spawnAndMount(Entity carrier, EntityType mount) {
-        T mountedEntity = (T) carrier.getLocation().getWorld().spawnEntity(carrier.getLocation(), mount);
+        T mountedEntity = spawnAndTagEntity(carrier.getLocation(), mount);
         carrier.addPassenger(mountedEntity);
         return mountedEntity;
     }
 
     @SuppressWarnings("unchecked")
     public static <T extends Entity> T spawnAndMount(EntityType carrier, Entity mount) {
-        T mountedEntity = (T) mount.getLocation().getWorld().spawnEntity(mount.getLocation(), carrier);
+        T mountedEntity = spawnAndTagEntity(mount.getLocation(), carrier);
         mount.addPassenger(mountedEntity);
         return mountedEntity;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Entity> T spawnAndTagEntity(Location location, EntityType entityType) {
+        Entity entity = location.getWorld().spawnEntity(location, entityType);
+        tagEntity(entity);
+        return (T) entity;
+    }
+
+    public static void tagEntity(Entity entity) {
+        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
+        dataContainer.set(BloodNight.getNamespacedKey("specialMob"), PersistentDataType.BYTE, (byte) 1);
+        if(entity instanceof LivingEntity){
+            ((LivingEntity) entity).setRemoveWhenFarAway(true);
+        }
+        entity.setPersistent(false);
+    }
+
+    public static boolean isSpecialMob(Entity entity) {
+        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
+        if (dataContainer.has(BloodNight.getNamespacedKey("specialMob"), PersistentDataType.BYTE)) {
+            Byte specialMob = dataContainer.get(BloodNight.getNamespacedKey("specialMob"), PersistentDataType.BYTE);
+            return specialMob != null && specialMob == (byte) 1;
+        }
+        return false;
     }
 }
