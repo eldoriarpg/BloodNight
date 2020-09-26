bddf6323e4d946938730e7fef14fc155914b4ded
RainbowDashLabs <mail@code-fox.de>
2020-09-26 02:19:01 +0200

    add about command

---
+++ src/main/java/de/eldoria/bloodnight/command/bloodnight/About.java
@@ -0,0 +1,34 @@
+package de.eldoria.bloodnight.command.bloodnight;
+
+import de.eldoria.eldoutilities.localization.Localizer;
+import de.eldoria.eldoutilities.localization.Replacement;
+import de.eldoria.eldoutilities.messages.MessageSender;
+import de.eldoria.eldoutilities.simplecommands.EldoCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.jetbrains.annotations.NotNull;
+
+public class About extends EldoCommand {
+    private final Plugin plugin;
+
+    public About(Localizer localizer, MessageSender messageSender, Plugin plugin) {
+        super(localizer, messageSender);
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        PluginDescriptionFile descr = plugin.getDescription();
+        String info = localizer().getMessage("about",
+                Replacement.create("PLUGIN_NAME", "Big Doors Opener").addFormatting('b'),
+                Replacement.create("AUTHORS", String.join(", ", descr.getAuthors())).addFormatting('b'),
+                Replacement.create("VERSION", descr.getVersion()).addFormatting('b'),
+                Replacement.create("WEBSITE", descr.getWebsite()).addFormatting('b'),
+                Replacement.create("DISCORD", "https://discord.gg/rfRuUge").addFormatting('b'));
+        messageSender().sendMessage(sender, info);
+        return true;
+    }
+
+}
