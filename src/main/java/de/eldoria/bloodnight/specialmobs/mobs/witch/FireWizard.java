46a57ba9de099d64810d757b2f094e5a4b2ea34d
RainbowDashLabs <mail@code-fox.de>
2020-10-05 12:57:32 +0200

    add projectile hit damage and projectile shoot to tick

--- src/main/java/de/eldoria/bloodnight/specialmobs/mobs/witch/FireWizard.java
+++ src/main/java/de/eldoria/bloodnight/specialmobs/mobs/witch/FireWizard.java
@@ -1,12 +1,25 @@
 package de.eldoria.bloodnight.specialmobs.mobs.witch;
 
 import de.eldoria.bloodnight.specialmobs.SpecialMobUtil;
+import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LargeFireball;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Witch;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class FireWizard extends AbstractWitch {
+    private Instant lastShot = Instant.now();
+
     public FireWizard(Witch witch) {
         super(witch);
         tick();
@@ -14,12 +27,26 @@ public class FireWizard extends AbstractWitch {
 
     @Override
     public void tick() {
-        SpecialMobUtil.spawnParticlesAround(getBaseEntity(), Particle.LAVA, 15);
+        EntityEquipment equipment = getBaseEntity().getEquipment();
+        equipment.setItemInMainHand(new ItemStack(Material.FIRE_CHARGE));
+        SpecialMobUtil.spawnParticlesAround(getBaseEntity(), Particle.DRIP_LAVA, 5);
+        if (lastShot.isBefore(Instant.now().minus(5, ChronoUnit.SECONDS))) {
+            SpecialMobUtil.launchProjectileOnTarget(getBaseEntity(), LargeFireball.class, 4);
+            lastShot = Instant.now();
+        }
     }
 
     @Override
     public void onProjectileShoot(ProjectileLaunchEvent event) {
+        if (event.getEntity().getType() == EntityType.FIREBALL) return;
         event.setCancelled(true);
-        SpecialMobUtil.launchProjectileOnTarget(getBaseEntity(), LargeFireball.class, 1.5);
+    }
+
+    @Override
+    public void onProjectileHit(ProjectileHitEvent event) {
+        if (event.getHitEntity().getType() == EntityType.PLAYER) {
+            AttributeInstance attribute = getBaseEntity().getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
+            ((LivingEntity) event.getHitEntity()).damage(attribute.getValue(), getBaseEntity());
+        }
     }
 }
