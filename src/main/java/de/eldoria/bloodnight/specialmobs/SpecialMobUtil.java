7842227dc11d4f645a952aac636ae0807193cb68
RainbowDashLabs <mail@code-fox.de>
2020-10-05 11:39:39 +0200

    add extension tag

--- src/main/java/de/eldoria/bloodnight/specialmobs/SpecialMobUtil.java
+++ src/main/java/de/eldoria/bloodnight/specialmobs/SpecialMobUtil.java
@@ -2,8 +2,10 @@ package de.eldoria.bloodnight.specialmobs;
 
 import de.eldoria.bloodnight.core.BloodNight;
 import de.eldoria.bloodnight.util.VectorUtil;
+import de.eldoria.eldoutilities.serialization.TypeConversion;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.entity.AreaEffectCloud;
@@ -23,11 +25,15 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 public final class SpecialMobUtil {
     private static final ThreadLocalRandom RAND = ThreadLocalRandom.current();
-
+    private static final NamespacedKey IS_SPECIAL_MOB = BloodNight.getNamespacedKey("isSpecialMob");
+    private static final NamespacedKey IS_MOB_EXTENSION = BloodNight.getNamespacedKey("isMobExtension");
+    private static final NamespacedKey BASE_UUID = BloodNight.getNamespacedKey("baseUUID");
+    private static final NamespacedKey MOB_TYPE = BloodNight.getNamespacedKey("mobType");
 
     private SpecialMobUtil() {
     }
@@ -109,6 +115,7 @@ public final class SpecialMobUtil {
     public static <T extends Entity> T spawnAndMount(Entity carrier, EntityType riderType) {
         T rider = spawnAndTagEntity(carrier.getLocation(), riderType);
         assert rider == null;
+        tagExtension(rider, carrier);
         carrier.addPassenger(rider);
         return rider;
     }
@@ -117,6 +124,7 @@ public final class SpecialMobUtil {
     public static <T extends Entity> T spawnAndMount(EntityType carrierType, Entity rider) {
         T carrier = spawnAndTagEntity(rider.getLocation(), carrierType);
         assert carrier == null;
+        tagExtension(carrier, rider);
         carrier.addPassenger(rider);
         return carrier;
     }
@@ -125,36 +133,87 @@ public final class SpecialMobUtil {
     public static <T extends Entity> T spawnAndTagEntity(Location location, EntityType entityType) {
         Entity entity = location.getWorld().spawnEntity(location, entityType);
         assert entity == null;
-        tagEntity(entity);
+        tagSpecialMob(entity);
         return (T) entity;
     }
 
-    public static void tagEntity(Entity entity) {
+    /**
+     * Marks a entity as a special mob extension
+     *
+     * @param entity   entity to mark
+     * @param extended the entity which is extended
+     */
+    public static void tagExtension(Entity entity, Entity extended) {
         PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
-        dataContainer.set(BloodNight.getNamespacedKey("specialMob"), PersistentDataType.BYTE, (byte) 1);
+        dataContainer.set(IS_MOB_EXTENSION, PersistentDataType.BYTE, (byte) 1);
+        dataContainer.set(BASE_UUID, PersistentDataType.BYTE_ARRAY,
+                TypeConversion.getBytesFromUUID(extended.getUniqueId()));
+    }
+
+    /**
+     * Checks if a entity is a extension of a special mob.
+     *
+     * @param entity entity to check
+     * @return true if the entity is a extension
+     */
+    public static boolean isExtension(Entity entity) {
+        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
+        if (dataContainer.has(IS_MOB_EXTENSION, PersistentDataType.BYTE)) {
+            Byte specialMob = dataContainer.get(BASE_UUID, PersistentDataType.BYTE);
+            return specialMob != null && specialMob == (byte) 1;
+        }
+        return false;
+    }
+
+    /**
+     * Get the UUID of the base mob.
+     * This will only return a UUID if {@link #isExtension(Entity)} returns true
+     *
+     * @param entity entity to check
+     * @return returns a uuid if the mob is a extension.
+     */
+    public static Optional<UUID> getBaseUUID(Entity entity) {
+        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
+        if (dataContainer.has(IS_MOB_EXTENSION, PersistentDataType.BYTE)) {
+            byte[] specialMob = dataContainer.get(BASE_UUID, PersistentDataType.BYTE_ARRAY);
+            return Optional.of(TypeConversion.getUUIDFromBytes(specialMob));
+        }
+        return Optional.empty();
+    }
+
+    public static void setSpecialMobType(Entity entity, String type) {
+        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
+        dataContainer.set(MOB_TYPE, PersistentDataType.STRING, type);
+    }
+
+    public static Optional<String> getSpecialMobType(Entity entity) {
+        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
+        if (dataContainer.has(MOB_TYPE, PersistentDataType.STRING)) {
+            return Optional.ofNullable(dataContainer.get(MOB_TYPE, PersistentDataType.STRING));
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Tags an entity as special mob.
+     * <p>This also sets {@link Entity#setPersistent(boolean)} to {@code false}
+     * and {@link LivingEntity#setRemoveWhenFarAway(boolean)} to {@code true}
+     *
+     * @param entity entity to tag
+     */
+    public static void tagSpecialMob(Entity entity) {
+        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
+        dataContainer.set(IS_SPECIAL_MOB, PersistentDataType.BYTE, (byte) 1);
         if (entity instanceof LivingEntity) {
             ((LivingEntity) entity).setRemoveWhenFarAway(true);
         }
         entity.setPersistent(false);
     }
 
-    public static void setSpecialMob(Entity entity, String name) {
-        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
-        dataContainer.set(BloodNight.getNamespacedKey("specialMobName"), PersistentDataType.STRING, name);
-    }
-
-    public static Optional<String> getSpecialMob(Entity entity) {
-        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
-        if (dataContainer.has(BloodNight.getNamespacedKey("specialMobName"), PersistentDataType.STRING)) {
-            return Optional.ofNullable(dataContainer.get(BloodNight.getNamespacedKey("specialMobName"), PersistentDataType.STRING));
-        }
-        return Optional.empty();
-    }
-
     public static boolean isSpecialMob(Entity entity) {
         PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
-        if (dataContainer.has(BloodNight.getNamespacedKey("specialMob"), PersistentDataType.BYTE)) {
-            Byte specialMob = dataContainer.get(BloodNight.getNamespacedKey("specialMob"), PersistentDataType.BYTE);
+        if (dataContainer.has(IS_SPECIAL_MOB, PersistentDataType.BYTE)) {
+            Byte specialMob = dataContainer.get(IS_SPECIAL_MOB, PersistentDataType.BYTE);
             return specialMob != null && specialMob == (byte) 1;
         }
         return false;
