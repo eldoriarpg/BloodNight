4cc60c37ff0613cd7c924de629f4af6b5f1d647d
Florian Fülling <mail@code-fox.de>
2020-10-15 19:29:35 +0200

    fix missing color code in message

--- src/main/java/de/eldoria/bloodnight/core/manager/NotificationManager.java
+++ src/main/java/de/eldoria/bloodnight/core/manager/NotificationManager.java
@@ -68,15 +68,16 @@ public class NotificationManager implements Listener {
     }
 
     private void sendMessage(Player player, String message) {
+        String m = "§a" + message.replace("§r", "§r§a");
         switch (configuration.getGeneralSettings().getBroadcastMethod()) {
             case CHAT:
                 messageSender.sendMessage(player, message);
                 break;
             case TITLE:
-                player.sendTitle("§a" + message, "", 10, 70, 20);
+                player.sendTitle(m, "", 10, 70, 20);
                 break;
             case SUBTITLE:
-                player.sendTitle("", "§a" + message, 10, 70, 20);
+                player.sendTitle("", m, 10, 70, 20);
                 break;
         }
     }
