12839e45f878cae5db6d5ae7c96c7c9ceffd8d66
RainbowDashLabs <mail@code-fox.de>
2020-11-11 00:46:31 +0100

    Move world settings to separate files.

--- src/main/java/de/eldoria/bloodnight/config/Configuration.java
+++ src/main/java/de/eldoria/bloodnight/config/Configuration.java
@@ -8,16 +8,26 @@ import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 
 public class Configuration {
     private final Plugin plugin;
     private final Map<String, WorldSettings> worldSettings = new HashMap<>();
+    private final Path worldSettingPaths;
+    private final Map<String, FileConfiguration> worldConfigs = new HashMap<>();
     /**
      * Version of the config.
      */
@@ -30,19 +40,16 @@ public class Configuration {
 
     public Configuration(Plugin plugin) {
         this.plugin = plugin;
+        worldSettingPaths = Paths.get(plugin.getDataFolder().toPath().toString(), "worldSettings");
         reload();
     }
 
-    public WorldSettings getWorldSettings(String key) {
-        if (worldSettings.containsKey(key)) {
-            return worldSettings.get(key);
-        }
-        BloodNight.logger().info("No world setting for " + key + " present. Computing default settings.");
-        return worldSettings.computeIfAbsent(key, WorldSettings::new);
+    public WorldSettings getWorldSettings(World key) {
+        return loadWorldSettings(key.getName(), false);
     }
 
-    public WorldSettings getWorldSettings(World key) {
-        return getWorldSettings(key.getName());
+    public WorldSettings getWorldSettings(String key) {
+        return loadWorldSettings(key, false);
     }
 
     public Map<String, WorldSettings> getWorldSettings() {
@@ -62,24 +69,107 @@ public class Configuration {
             config = plugin.getConfig();
         }
 
-
         version = config.getInt("version");
-        generalSettings = (GeneralSettings) config.get("generalSettings", new GeneralSettings());
-        worldSettings.clear();
-        List<WorldSettings> worldList = ObjUtil.nonNull((List<WorldSettings>) config.get("worldSettings", new ArrayList<>()), new ArrayList<>());
-        for (WorldSettings settings : worldList) {
-            worldSettings.put(settings.getWorldName(), settings);
-            if (generalSettings.isDebug()) {
-                BloodNight.logger().info("Loading world settings for " + settings.getWorldName());
-            }
+
+        if (version <= 1) {
+            BloodNight.logger().info("Migrating config to v2");
+            migrateToV2();
+            BloodNight.logger().info("Migration of config to v2 done.");
         }
 
+        generalSettings = config.getObject("generalSettings", GeneralSettings.class);
+
         for (World world : Bukkit.getWorlds()) {
-            getWorldSettings(world);
+            loadWorldSettings(world.getName(), true);
         }
 
         saveConfig();
+    }
 
+    private void migrateToV2() {
+        FileConfiguration config = plugin.getConfig();
+        config.set("version", 2);
+        List<WorldSettings> worldList = ObjUtil.nonNull((List<WorldSettings>) config.get("worldSettings", new ArrayList<>()), new ArrayList<>());
+
+        Path worldSettings = Paths.get(plugin.getDataFolder().toPath().toString(), "worldSettings");
+        try {
+            Files.createDirectories(worldSettings);
+        } catch (IOException e) {
+            BloodNight.logger().log(Level.WARNING, "Could not create world settings directory.", e);
+        }
+
+        for (WorldSettings settings : worldList) {
+            Path worldSetting = Paths.get(worldSettings.toString(), settings.getWorldName() + ".yml");
+            File worldConfig = worldSetting.toFile();
+            YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(worldConfig);
+
+            if (!worldConfig.exists()) {
+                createWorldConfigFile(settings.getWorldName(), settings);
+                BloodNight.logger().info("Migrated settings for " + settings.getWorldName());
+            }
+        }
+        config.set("worldSettings", null);
+    }
+
+    private WorldSettings loadWorldSettings(String world, boolean reload) {
+        if (reload) {
+            return worldSettings.compute(world, (k, v) -> {
+                FileConfiguration config = worldConfigs.compute(
+                        k, (k2, v2) -> {
+                            Path worldSetting = Paths.get(worldSettingPaths.toString(), world + ".yml");
+                            if (!worldSetting.toFile().exists()) {
+                                createWorldConfigFile(world, null);
+                            }
+
+                            return YamlConfiguration.loadConfiguration(worldSetting.toFile());
+                        });
+                return config.getObject("settings", WorldSettings.class);
+            });
+        }
+        return worldSettings.computeIfAbsent(world, k -> {
+            FileConfiguration config = worldConfigs.computeIfAbsent(k, k2 -> {
+                Path worldSetting = Paths.get(worldSettingPaths.toString(), world + ".yml");
+                if (!worldSetting.toFile().exists()) {
+                    createWorldConfigFile(world, null);
+                }
+
+                return YamlConfiguration.loadConfiguration(worldSetting.toFile());
+            });
+            return config.getObject("settings", WorldSettings.class);
+        });
+    }
+
+    private FileConfiguration createWorldConfigFile(String world, @Nullable WorldSettings settings) {
+        Path worldSetting = Paths.get(worldSettingPaths.toString(), world + ".yml");
+
+        try {
+            Files.createFile(worldSetting);
+        } catch (IOException e) {
+            BloodNight.logger().log(Level.WARNING, "Could not create world settings.", e);
+            return null;
+        }
+
+        YamlConfiguration tempConfig = YamlConfiguration.loadConfiguration(worldSetting.toFile());
+        tempConfig.set("version", 1);
+        tempConfig.set("settings", ObjUtil.nonNull(settings, new WorldSettings(world)));
+        try {
+            tempConfig.save(worldSetting.toFile());
+        } catch (IOException e) {
+            BloodNight.logger().log(Level.WARNING, "Could not save config.");
+        }
+        return tempConfig;
+    }
+
+    private void saveWorldConfig(String world) {
+        Path path = Paths.get(worldSettingPaths.toString(), world + ".yml");
+        WorldSettings worldSettings = getWorldSettings(world);
+        FileConfiguration config = worldConfigs.get(world);
+        config.set("settings", worldSettings);
+        try {
+            config.save(path.toFile());
+        } catch (IOException e) {
+            BloodNight.logger().log(Level.WARNING, "Could not save world settings", e);
+        }
     }
 
     private void init() {
@@ -98,7 +188,11 @@ public class Configuration {
         FileConfiguration config = plugin.getConfig();
         config.set("version", version);
         config.set("generalSettings", generalSettings);
-        config.set("worldSettings", new ArrayList<>(worldSettings.values()));
+
+        for (String world : worldConfigs.keySet()) {
+            saveWorldConfig(world);
+        }
+
         if (generalSettings.isDebug()) {
             BloodNight.logger().info("Saved config.");
         }
