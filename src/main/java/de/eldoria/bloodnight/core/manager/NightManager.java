95f139b85854ba7cfae9f343b5c09ef07e0981d6
RainbowDashLabs <mail@code-fox.de>
2020-11-11 21:40:42 +0100

    missing import

--- src/main/java/de/eldoria/bloodnight/core/manager/NightManager.java
+++ src/main/java/de/eldoria/bloodnight/core/manager/NightManager.java
@@ -25,6 +25,7 @@ import org.bukkit.boss.BossBar;
 import org.bukkit.boss.KeyedBossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
