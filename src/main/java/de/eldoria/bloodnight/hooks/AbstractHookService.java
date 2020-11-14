92cec28a9357dec89266cc8f16bff0e522645945
RainbowDashLabs <mail@code-fox.de>
2020-11-14 16:45:11 +0100

    Add abstract hook service

---
+++ src/main/java/de/eldoria/bloodnight/hooks/AbstractHookService.java
@@ -0,0 +1,42 @@
+package de.eldoria.bloodnight.hooks;
+
+import lombok.Getter;
+import org.bukkit.Bukkit;
+
+/**
+ * A hook for a plugin.
+ *
+ * @param <T> type of plugin hook.
+ */
+public abstract class AbstractHookService<T> {
+    private final String name;
+    private boolean initialized;
+    /**
+     * True if the plugin of this hook is enabled.
+     */
+    @Getter
+    private final boolean active;
+
+    public AbstractHookService(String name) {
+        this.name = name;
+        active = Bukkit.getPluginManager().isPluginEnabled(name);
+    }
+
+    /**
+     * Get the Hook of the Plugin.
+     *
+     * @return hook instance.
+     * @throws ClassNotFoundException when the plugin is not loaded.
+     */
+    public abstract T getHook() throws ClassNotFoundException;
+
+    /**
+     * Initialize the hook
+     */
+    public abstract void setup();
+
+    /**
+     * Shutdown the hook and stop any attached services.
+     */
+    public abstract void shutdown();
+}
