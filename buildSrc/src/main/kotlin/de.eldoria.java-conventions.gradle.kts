5183b4ae4f7f04d210c184ad6a25c5a37cc4fe3e
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-05-21 15:18:25 +0200

    Adjust repositories to use eldonexus proxies

--- buildSrc/src/main/kotlin/de.eldoria.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/de.eldoria.java-conventions.gradle.kts
@@ -9,10 +9,8 @@ java.sourceCompatibility = JavaVersion.VERSION_1_8
 val lombokVersion = "1.18.20"
 
 repositories {
-    maven { url = uri("https://eldonexus.de/repository/maven-releases/") }
-    maven { url = uri("https://eldonexus.de/repository/maven-snapshots/") }
-    maven { url = uri("https://eldonexus.de/repository/spigot/") }
-    maven { url = uri("https://eldonexus.de/repository/OnARandomBox/") }
+    maven { url = uri("https://eldonexus.de/repository/maven-public/") }
+    maven { url = uri("https://eldonexus.de/repository/maven-proxies/") }
     maven { url = uri("https://repo.maven.apache.org/maven2/") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
     maven { url = uri("https://mvn.lumine.io/repository/maven-public/") }
