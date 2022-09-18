f7bea5c00f3fa540d002114d55de6828d15b4d8b
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-09-18 14:51:47 +0200

    Small adjustments on KnightZombie

--- BloodNight-core/src/main/java/de/eldoria/bloodnight/specialmobs/mobs/zombie/KnightZombie.java
+++ BloodNight-core/src/main/java/de/eldoria/bloodnight/specialmobs/mobs/zombie/KnightZombie.java
@@ -5,11 +5,9 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -17,9 +15,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class KnightZombie extends AbstractZombie {
 
-    public KnightZombie(Zombie zombie){
+    public KnightZombie(Zombie zombie) {
         super(zombie);
-        EntityEquipment equipment = zombie.getEquipment();
+        var equipment = zombie.getEquipment();
         if (equipment != null) {
             equipment.setHelmet(new ItemStack(Material.CHAINMAIL_HELMET, 1));
             equipment.setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE, 1));
@@ -28,26 +26,29 @@ public class KnightZombie extends AbstractZombie {
             equipment.setItemInMainHand(new ItemStack(Material.GOLDEN_AXE, 1));
         }
     }
+
     @Override
     public void onHit(@NotNull EntityDamageByEntityEvent e) {
         if (e.getEntity() instanceof Player) {
-            if (ThreadLocalRandom.current().nextDouble() <= 0.15) { //changes the chance to increase/decrease the custom push
-                e.setCancelled(true);
-                Player player = (Player) e.getEntity();
-                player.setVelocity(new Vector(1, 1, 1)); //pushes the player up into the air
-                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 2, true, true, true));
-                player.playSound(player.getLocation(), Sound.BLOCK_BUBBLE_COLUMN_BUBBLE_POP, 15, 3);
-            }
+            //changes the chance to increase/decrease the custom push
+            if (ThreadLocalRandom.current().nextDouble() > 0.15) return;
+            e.setCancelled(true);
+            var player = (Player) e.getEntity();
+            //pushes the player up into the air
+            player.getVelocity().setY(Math.max(player.getVelocity().getY() + 1, 1));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 2, true, true, true));
+            player.playSound(player.getLocation(), Sound.BLOCK_BUBBLE_COLUMN_BUBBLE_POP, 15, 3);
         }
     }
+
     @Override
     public void onDamageByEntity(@NotNull EntityDamageByEntityEvent e) {
         if (e.getDamager() instanceof Player) {
-            if ((ThreadLocalRandom.current().nextDouble() <= 0.12)) { //changes the chance to increase/decrease the block the attack
-                e.setCancelled(true);
-                Player player = (Player) e.getDamager();
-                player.playSound(player.getLocation(), Sound.ITEM_SHIELD_BLOCK, 15, 1);
-            }
+            //changes the chance to increase/decrease the block the attack
+            if ((ThreadLocalRandom.current().nextDouble() > 0.12)) return;
+            e.setCancelled(true);
+            var player = (Player) e.getDamager();
+            player.playSound(player.getLocation(), Sound.ITEM_SHIELD_BLOCK, 15, 1);
         }
     }
 }
